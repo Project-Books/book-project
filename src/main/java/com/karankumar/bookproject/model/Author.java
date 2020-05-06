4ed9f4ee9bad86ee4c3570a2ed9d9a51fc54d381
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-06 14:05:16 +0100

    add author POJO

--- src/main/java/com/karankumar/bookproject/model/Author.java
+++ src/main/java/com/karankumar/bookproject/model/Author.java
@@ -1,7 +1,45 @@
 package com.karankumar.bookproject.model;
 
+import java.util.Set;
+
 /**
  * @author karan on 06/05/2020
  */
 public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
