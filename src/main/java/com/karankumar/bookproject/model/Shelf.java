f17f4c514398c6dfc2ea04c9ec321500bdc61250
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-06 14:05:27 +0100

    add shelf POJO

--- src/main/java/com/karankumar/bookproject/model/Shelf.java
+++ src/main/java/com/karankumar/bookproject/model/Shelf.java
@@ -1,7 +1,36 @@
 package com.karankumar.bookproject.model;
 
+import java.util.Set;
+
 /**
  * @author karan on 06/05/2020
  */
 public class Shelf {
+    private Long id;
+    private String name;
+    private Set<Book> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
