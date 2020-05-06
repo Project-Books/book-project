4e4f3adb81bdd8522a80e54727194ae61de10a93
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-06 11:33:33 +0100

    add maven project

---
+++ src/main/java/com/karankumar/bookproject/BookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.karankumar.bookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karankumar/bookproject/BookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karankumar.bookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
