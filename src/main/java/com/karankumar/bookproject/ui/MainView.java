239a77fee0fac912a4ea67777a7681736c64b913
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-09 16:20:05 +0100

    add MainView

---
+++ src/main/java/com/karankumar/bookproject/ui/MainView.java
@@ -0,0 +1,16 @@
+package com.karankumar.bookproject.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.jboss.jandex.Main;
+
+/**
+ * @author karan on 09/05/2020
+ */
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
