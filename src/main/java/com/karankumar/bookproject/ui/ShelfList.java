0b891daa027ed35ca4ae722dbb809df19d1f2ed0
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-10 14:49:52 +0100

    add grid of shelves (WIP)

--- src/main/java/com/karankumar/bookproject/ui/ShelfList.java
+++ src/main/java/com/karankumar/bookproject/ui/ShelfList.java
@@ -1,7 +1,29 @@
 package com.karankumar.bookproject.ui;
 
+import com.karankumar.bookproject.backend.model.Shelf;
+import com.karankumar.bookproject.backend.service.ShelfService;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 /**
  * @author karan on 10/05/2020
  */
-public class ShelfList {
+public class ShelfList extends VerticalLayout {
+
+    private Grid<Shelf> grid = new Grid<>(Shelf.class);
+    private ShelfService shelfService;
+
+    public ShelfList(ShelfService shelfService) {
+        this.shelfService = shelfService;
+        addClassName("shelf-list");
+        configureGrid();
+        add(grid);
+
+        grid.setItems(shelfService.findAll());
+    }
+
+    private void configureGrid() {
+        grid.setColumns("name");
+    }
+
 }
