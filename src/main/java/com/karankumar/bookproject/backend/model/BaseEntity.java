6487372649ceb0e70f7375ae37070f1f164f0323
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-13 13:56:33 +0100

    add base entity

---
+++ src/main/java/com/karankumar/bookproject/backend/model/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.karankumar.bookproject.backend.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author karan on 13/05/2020
+ */
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private Long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() != null) {
+            return getId().hashCode();
+        }
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        BaseEntity other = (BaseEntity) obj;
+        if (getId() == null || other.getId() == null) {
+            return false;
+        }
+        return getId().equals(other.getId());
+    }
+}
