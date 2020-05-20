425e44c612e292f0f3fb4924fad367332d2474fb
Taciano Morais Silva <tacianosilva@gmail.com>
2020-05-20 12:50:08 -0300

    RatingScale shown now as a fraction out of 10. #32
    For example, if the rating is NINE_POINT_FIVE, it should be displayed as
    9.5/10.

--- src/main/java/com/karankumar/bookproject/backend/model/RatingScale.java
+++ src/main/java/com/karankumar/bookproject/backend/model/RatingScale.java
@@ -22,25 +22,36 @@ package com.karankumar.bookproject.backend.model;
  * A rating scale for a {@code Book} from 0 to 10 (inclusive) that goes up in increments of 0.5.
  */
 public enum RatingScale {
-    ZERO,
-    ZERO_POINT_FIVE,
-    ONE,
-    ONE_POINT_FIVE,
-    TWO,
-    TWO_POINT_FIVE,
-    THREE,
-    THREE_POINT_FIVE,
-    FOUR,
-    FOUR_POINT_FIVE,
-    FIVE,
-    FIVE_POINT_FIVE,
-    SIX,
-    SIX_POINT_FIVE,
-    SEVEN,
-    SEVEN_POINT_FIVE,
-    EIGHT,
-    EIGHT_POINT_FIVE,
-    NINE,
-    NINE_POINT_FIVE,
-    TEN
+    ZERO("0/10"),
+    ZERO_POINT_FIVE("0.5/10"),
+    ONE("1/10"),
+    ONE_POINT_FIVE("1.5/10"),
+    TWO("2/10"),
+    TWO_POINT_FIVE("2.5/10"),
+    THREE("3/10"),
+    THREE_POINT_FIVE("3.5/10"),
+    FOUR("4/10"),
+    FOUR_POINT_FIVE("4.5/10"),
+    FIVE("5/10"),
+    FIVE_POINT_FIVE("5.5/10"),
+    SIX("6/10"),
+    SIX_POINT_FIVE("6.5/10"),
+    SEVEN("7/10"),
+    SEVEN_POINT_FIVE("7.5/10"),
+    EIGHT("8/10"),
+    EIGHT_POINT_FIVE("8.5/10"),
+    NINE("9/10"),
+    NINE_POINT_FIVE("9.5/10"),
+    TEN("10/10");
+
+    private String rating;
+
+    RatingScale(String rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return rating;
+    }
 }
