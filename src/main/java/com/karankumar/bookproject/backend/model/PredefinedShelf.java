b8b9fda0d5dada944db6915c459ed1b1a1d0068e
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-06-18 11:04:52 +0100

    change enum type from ordinal to string

--- src/main/java/com/karankumar/bookproject/backend/model/PredefinedShelf.java
+++ src/main/java/com/karankumar/bookproject/backend/model/PredefinedShelf.java
@@ -1,8 +1,6 @@
 package com.karankumar.bookproject.backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
@@ -26,6 +24,7 @@ public class PredefinedShelf extends BaseEntity {
     }
 
     @NotNull
+    @Enumerated(value = EnumType.STRING)
     public ShelfName shelfName;
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "shelf")
