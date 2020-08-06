b231b6cee2a728b8efdceaf4a19d40f711347eb3
Karan Kumar <karan@karankumar.com>
2020-08-06 19:38:55 +0100

    add stubs

---
+++ src/test/java/com/karankumar/bookproject/backend/statistics/GenreStatisticsTest.java
@@ -0,0 +1,24 @@
+package com.karankumar.bookproject.backend.statistics;
+
+import com.karankumar.bookproject.tags.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+public class GenreStatisticsTest {
+
+    @Test
+    public void mostReadGenreExistsAndIsFound() {
+        // TODO
+    }
+
+    @Test
+    public void mostLikedGenreExistsAndIsFound() {
+        // TODO
+    }
+
+    @Test
+    public void leastLikedGenreExistsAndIsFound() {
+        // TODO
+    }
+
+}
---
+++ src/test/java/com/karankumar/bookproject/backend/statistics/PageStatisticsTest.java
@@ -0,0 +1,23 @@
+package com.karankumar.bookproject.backend.statistics;
+
+import com.karankumar.bookproject.tags.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+public class PageStatisticsTest {
+
+    @Test
+    public void bookWithMostPagesExistsAndIsFound() {
+        // TODO
+    }
+
+    @Test
+    public void testAveragePageLengthDivideByZero() {
+        // TODO
+    }
+
+    @Test
+    public void onlyReadBooksCountTowardsStatistics() {
+        // TODO
+    }
+}
