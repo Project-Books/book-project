bbe5627b475b1528223e8a7b9df7121fc8701cdd
Karan Kumar <karan@karankumar.com>
2020-08-09 17:07:40 +0100

    add tests for invalid passwords

--- src/test/java/com/karankumar/bookproject/backend/entity/UserTest.java
+++ src/test/java/com/karankumar/bookproject/backend/entity/UserTest.java
@@ -4,6 +4,7 @@ import com.karankumar.bookproject.annotations.IntegrationTest;
 import com.karankumar.bookproject.backend.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.TransactionSystemException;
@@ -11,46 +12,94 @@ import org.springframework.transaction.TransactionSystemException;
 @IntegrationTest
 public class UserTest {
     private static UserRepository userRepository;
+    private long initialNumberOfUsers;
 
     @BeforeAll
     public static void setup(@Autowired UserRepository userRepository) {
         UserTest.userRepository = userRepository;
     }
 
+    @BeforeEach
+    void beforeEachSetup() {
+        initialNumberOfUsers = UserTest.userRepository.count();
+    }
+
     @Test
     void testValidUserSaved() {
         // given
-        long numberOfUsers = userRepository.count();
+        User user = userWithoutPassword().password("passwordP1&")
+                                         .build();
 
         // when
-        User user = userWithoutPassword().password("passwordP12&")
-                                         .build();
         userRepository.save(user);
 
         // then
-        Assertions.assertEquals(numberOfUsers + 1, userRepository.count());
+        Assertions.assertEquals(initialNumberOfUsers + 1, userRepository.count());
     }
 
     private User.UserBuilder userWithoutPassword() {
         return User.builder()
-                .username("username")
-                .email("email");
+                   .username("username")
+                   .email("email");
     }
 
     @Test
     void testPasswordLessThan8CharactersIsInvalid() {
         // given
-        long numberOfUsers = userRepository.count();
-
-        // when
-        User user = userWithoutPassword().password("pP12&")
+        User user = userWithoutPassword().password("pP1&")
                                          .build();
 
+        // when
+        tryToSaveInvalidUser(user);
+
+        // then
+        Assertions.assertEquals(initialNumberOfUsers, userRepository.count());
+    }
+
+    void tryToSaveInvalidUser(User user) {
         try {
+            // when
             userRepository.save(user);
         } catch (TransactionSystemException expected) {
         }
+    }
+
+    @Test
+    void testPasswordWithNoDigitIsInvalid() {
+        // given
+        User user = userWithoutPassword().password("passwordP&")
+                                         .build();
+
+        // when
+        tryToSaveInvalidUser(user);
+
         // then
-        Assertions.assertEquals(numberOfUsers, userRepository.count());
+        Assertions.assertEquals(initialNumberOfUsers, userRepository.count());
+    }
+
+    @Test
+    void testPasswordWithNoUppercaseCharacterIsInvalid() {
+        // given
+        User user = userWithoutPassword().password("password1&")
+                                         .build();
+
+        // when
+        tryToSaveInvalidUser(user);
+
+        // then
+        Assertions.assertEquals(initialNumberOfUsers, userRepository.count());
+    }
+
+    @Test
+    void testPasswordWithNoSpecialCharacterIsInvalid() {
+        // given
+        User user = userWithoutPassword().password("passwordP1")
+                                         .build();
+
+        // when
+        tryToSaveInvalidUser(user);
+
+        // then
+        Assertions.assertEquals(initialNumberOfUsers, userRepository.count());
     }
 }
