2ae49e61328ae4380255b833a363cda6a3214331
kaansonmezoz <ksonmezoz@outlook.com>
2020-08-13 01:31:57 +0300

    Replace * imports by explicit imports

--- src/main/java/com/karankumar/bookproject/backend/entity/account/User.java
+++ src/main/java/com/karankumar/bookproject/backend/entity/account/User.java
@@ -15,9 +15,25 @@
 
 package com.karankumar.bookproject.backend.entity.account;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
