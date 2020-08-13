cec5d35b6fa21b7a0e07577b93226922a0611577
kaansonmezoz <ksonmezoz@outlook.com>
2020-08-13 21:03:09 +0300

    Re-order imports

--- src/test/java/com/karankumar/bookproject/ui/MockSpringServlet.java
+++ src/test/java/com/karankumar/bookproject/ui/MockSpringServlet.java
@@ -23,11 +23,10 @@ import com.vaadin.flow.server.VaadinServletService;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.SpringServlet;
 import com.vaadin.flow.spring.SpringVaadinServletService;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.ApplicationContext;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ApplicationContext;
 
 public class MockSpringServlet extends SpringServlet {
     @NotNull
