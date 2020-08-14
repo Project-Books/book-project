1b6b138bff7e03a9a5c7c9dd93f8a8ba24f52645
Christoph Flick <ChFlick@users.noreply.github.com>
2020-08-14 13:13:16 +0200

    Add the registration frontend view

---
+++ src/main/java/com/karankumar/bookproject/ui/registration/RegistrationView.java
@@ -0,0 +1,40 @@
+/*
+    The book project lets a user keep track of different books they've read, are currently reading or would like to read
+    Copyright (C) 2020  Karan Kumar
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.karankumar.bookproject.ui.registration;
+
+import com.karankumar.bookproject.backend.service.UserService;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("register")
+@PageTitle("Register | Book Project")
+public class RegistrationView extends VerticalLayout {
+    public RegistrationView(UserService userService) {
+        addClassName("registration-view");
+        setSizeFull();
+
+        // centre
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignItems(Alignment.CENTER);
+
+        add(
+                new H1("Book Project"),
+                new RegistrationForm(userService)
+        );
+    }
+}
