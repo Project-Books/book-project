8d78dce6abac576616ea735448b47e5dbe529dc0
Karan <karan@karankumar.com>
2020-08-22 07:55:24 +0100

    remove unused Role imports

--- src/main/java/com/karankumar/bookproject/backend/entity/account/Role.java
+++ src/main/java/com/karankumar/bookproject/backend/entity/account/Role.java
@@ -21,15 +21,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
