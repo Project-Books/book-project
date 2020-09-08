ea83ba33de4b72e6faaaf1c06f0fe305fdacefed
kaansonmezoz <ksonmezoz@outlook.com>
2020-09-08 12:52:11 +0300

    Add mapping value for RatingScale

--- src/main/java/com/karankumar/bookproject/backend/entity/RatingScale.java
+++ src/main/java/com/karankumar/bookproject/backend/entity/RatingScale.java
@@ -19,33 +19,35 @@
 package com.karankumar.bookproject.backend.entity;
 
 public enum RatingScale {
-    NO_RATING("No rating"),
-    ZERO("0/10"),
-    ZERO_POINT_FIVE("0.5/10"),
-    ONE("1/10"),
-    ONE_POINT_FIVE("1.5/10"),
-    TWO("2/10"),
-    TWO_POINT_FIVE("2.5/10"),
-    THREE("3/10"),
-    THREE_POINT_FIVE("3.5/10"),
-    FOUR("4/10"),
-    FOUR_POINT_FIVE("4.5/10"),
-    FIVE("5/10"),
-    FIVE_POINT_FIVE("5.5/10"),
-    SIX("6/10"),
-    SIX_POINT_FIVE("6.5/10"),
-    SEVEN("7/10"),
-    SEVEN_POINT_FIVE("7.5/10"),
-    EIGHT("8/10"),
-    EIGHT_POINT_FIVE("8.5/10"),
-    NINE("9/10"),
-    NINE_POINT_FIVE("9.5/10"),
-    TEN("10/10");
+    NO_RATING("No rating", null),
+    ZERO("0/10", 0.0),
+    ZERO_POINT_FIVE("0.5/10", 0.5),
+    ONE("1/10", 1.0),
+    ONE_POINT_FIVE("1.5/10", 1.5),
+    TWO("2/10", 2.0),
+    TWO_POINT_FIVE("2.5/10", 2.5),
+    THREE("3/10", 3.0),
+    THREE_POINT_FIVE("3.5/10", 3.5),
+    FOUR("4/10", 4.0),
+    FOUR_POINT_FIVE("4.5/10", 4.5),
+    FIVE("5/10", 5.0),
+    FIVE_POINT_FIVE("5.5/10", 5.5),
+    SIX("6/10", 6.0),
+    SIX_POINT_FIVE("6.5/10", 6.5),
+    SEVEN("7/10", 7.0),
+    SEVEN_POINT_FIVE("7.5/10", 7.5),
+    EIGHT("8/10", 8.0),
+    EIGHT_POINT_FIVE("8.5/10", 8.5),
+    NINE("9/10", 9.0),
+    NINE_POINT_FIVE("9.5/10", 9.5),
+    TEN("10/10", 10.0);
 
     private String rating;
+    private Double value;
 
-    RatingScale(String rating) {
+    RatingScale(String rating, Double value) {
         this.rating = rating;
+        this.value = value;
     }
 
     @Override
