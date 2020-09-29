7b92c01bf37ec41e5132c7118bbafed27d4bdc42
Karan <karan@karankumar.com>
2020-09-29 19:02:34 +0100

    extract conditions to methods

--- src/main/java/com/karankumar/bookproject/backend/goal/CalculateReadingGoal.java
+++ src/main/java/com/karankumar/bookproject/backend/goal/CalculateReadingGoal.java
@@ -64,13 +64,17 @@ public class CalculateReadingGoal {
         boolean lookingForBooks = goalType.equals(GoalType.BOOKS);
         for (Book book : readShelf.getBooks()) {
             // only books that have been given a finish date can count towards the reading goal
-            if (book != null && book.getDateFinishedReading() != null
-                    && DateUtils.dateIsInCurrentYear(book.getDateFinishedReading())) {
+            if (bookHasFinishDateInThisYear(book)) {
                 int pages = (book.getNumberOfPages() == null) ? 0 : book.getNumberOfPages();
                 readThisYear += (lookingForBooks ? (1) : pages);
             }
         }
         return readThisYear;
+    }
+
+    private static boolean bookHasFinishDateInThisYear(Book book) {
+        return book != null && book.getDateFinishedReading() != null &&
+                DateUtils.dateIsInCurrentYear(book.getDateFinishedReading());
     }
 
     public static int howFarAheadOrBehindSchedule(int booksToReadThisYear, int booksReadThisYear) {
