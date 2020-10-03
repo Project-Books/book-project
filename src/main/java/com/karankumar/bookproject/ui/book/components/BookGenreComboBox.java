8fa4765c973982ffa2c5d6911c27ceb5588fe78d
kaansonmezoz <ksonmezoz@outlook.com>
2020-10-03 15:35:40 +0300

    Extended FormItem

--- src/main/java/com/karankumar/bookproject/ui/book/components/BookGenreComboBox.java
+++ src/main/java/com/karankumar/bookproject/ui/book/components/BookGenreComboBox.java
@@ -2,45 +2,43 @@ package com.karankumar.bookproject.ui.book.components;
 
 import com.karankumar.bookproject.backend.entity.Book;
 import com.karankumar.bookproject.backend.entity.Genre;
+import com.karankumar.bookproject.ui.book.components.form.item.FormItem;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.data.binder.Binder;
 
-public class BookGenreComboBox {
-
-    private final ComboBox<Genre> component;
+public class BookGenreComboBox extends FormItem<ComboBox<Genre>> {
 
     public BookGenreComboBox() {
-        this.component = new ComboBox<>();
+        super(new ComboBox<>());
     }
 
+    @Override
     public void configure() {
+        ComboBox<Genre> component = getField();
         component.setItems(Genre.values());
         component.setPlaceholder("Choose a book genre");
     }
 
-    public void add(FormLayout layout) {
-        layout.addFormItem(component, "Book genre");
+    @Override
+    protected String getLabel(){
+        return "Book genre";
+    }
+
+    @Override
+    public void bind(Binder<Book> binder, ComboBox<Genre> fieldToCompare) {
+        binder.forField(super.getField())
+                .bind(Book::getGenre, Book::setGenre);
     }
 
     public Genre getValue() {
-        return component.getValue();
-    }
-
-    public ComboBox<Genre> getComponent() {
-        return component;
+        return super.getField().getValue();
     }
 
     public void setValue(Genre genre) {
-        component.setValue(genre);
+        super.getField().setValue(genre);
     }
 
     public boolean isEmpty() {
-        return component.isEmpty();
-    }
-
-    public void bind(Binder<Book> binder) {
-        binder.forField(component)
-              .bind(Book::getGenre, Book::setGenre);
+        return super.getField().isEmpty();
     }
 }
