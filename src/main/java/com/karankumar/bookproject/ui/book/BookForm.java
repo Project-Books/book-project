c759a543dd4a76379a16bb17d2a123d68013e32f
Karan <karan@karankumar.com>
2020-10-17 13:36:01 +0100

    extract repopulating to new method

--- src/main/java/com/karankumar/bookproject/ui/book/BookForm.java
+++ src/main/java/com/karankumar/bookproject/ui/book/BookForm.java
@@ -355,13 +355,7 @@ public class BookForm extends VerticalLayout {
     }
 
     private void setBookBean() {
-        // Only repopulate if we create a new Book
-        if (binder.getBean() == null) {
-            Book book = populateBookBean();
-            if (book != null) {
-                binder.setBean(book);
-            }
-        }
+        repopulateIfCreatingANewBook();
 
         if (binder.getBean() != null) {
             LOGGER.log(Level.INFO, "Written bean. Not Null.");
@@ -373,6 +367,15 @@ public class BookForm extends VerticalLayout {
             showErrorMessage();
         }
         closeForm();
+    }
+
+    private void repopulateIfCreatingANewBook() {
+        if (binder.getBean() == null) {
+            Book book = populateBookBean();
+            if (book != null) {
+                binder.setBean(book);
+            }
+        }
     }
 
     private Book populateBookBean() {
