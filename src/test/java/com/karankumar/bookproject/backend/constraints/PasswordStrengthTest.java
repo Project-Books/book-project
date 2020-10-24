2dd6c225229878e0cac90c8233929d2809f1ba76
Karan <karan@karankumar.com>
2020-10-24 17:40:02 +0100

    test byNum()

---
+++ src/test/java/com/karankumar/bookproject/backend/constraints/PasswordStrengthTest.java
@@ -0,0 +1,27 @@
+package com.karankumar.bookproject.backend.constraints;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+@DisplayName("PasswordStrength should")
+class PasswordStrengthTest {
+    @Test
+    void notAllowOutOfBoundValues() {
+        // given
+        int belowArrayLowerBound = -1;
+        int aboveArrayUpperBound = PasswordStrength.values().length + 1;
+
+        // when
+        Throwable thrown1 = catchThrowable(() -> PasswordStrength.byNum(belowArrayLowerBound));
+        Throwable thrown2 = catchThrowable(() -> PasswordStrength.byNum(aboveArrayUpperBound));
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(thrown1).isInstanceOf(IllegalArgumentException.class);
+            softly.assertThat(thrown2).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+}
