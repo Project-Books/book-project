ae7093966b9c9db5b2402bcb0ec9b02201d34818
mareksmelko777 <mareksmelko777@gmail.com>
2020-11-04 14:11:13 +0100

    fixing stylistics

--- src/main/java/com/karankumar/bookproject/ui/goal/ComponentGoalEvent.java
+++ src/main/java/com/karankumar/bookproject/ui/goal/ComponentGoalEvent.java
@@ -1,3 +1,20 @@
+/*
+    The book project lets a user keep track of different books they would like to read, are currently
+    reading, have read or did not finish.
+    Copyright (C) 2020  Karan Kumar
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    GNU General Public License as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+    PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.karankumar.bookproject.ui.goal;
 
 import com.karankumar.bookproject.backend.entity.ReadingGoal;
@@ -5,15 +22,15 @@ import com.vaadin.flow.component.ComponentEvent;
 
 public class ComponentGoalEvent {
     public abstract static class GoalFormEvent extends ComponentEvent<ReadingGoalForm> {
-    private final ReadingGoal readingGoal;
+        private final ReadingGoal readingGoal;
 
-    protected GoalFormEvent(ReadingGoalForm source, ReadingGoal readingGoal) {
-        super(source, false);
-        this.readingGoal = readingGoal;
-    }
+        protected GoalFormEvent(ReadingGoalForm source, ReadingGoal readingGoal) {
+            super(source, false);
+            this.readingGoal = readingGoal;
+        }
 
-    public ReadingGoal getReadingGoal() {
-        return readingGoal;
+        public ReadingGoal getReadingGoal() {
+            return readingGoal;
+        }
     }
-}
 }
