c83aafb0bbc84bfabf18ca1fa809e60b238e7b29
Rahul Yadav <rahulyadav7477@gmail.com>
2020-11-09 00:07:20 +0530

    Added CsvUtilsTest and goodreadsImportSample.csv

---
+++ src/test/java/com/karankumar/bookproject/backend/utils/CsvUtilsTest.java
@@ -0,0 +1,25 @@
+package com.karankumar.bookproject.backend.utils;
+
+import com.karankumar.bookproject.backend.dto.GoodreadsBookImport;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CsvUtilsTest {
+
+    @Test
+    void testReadWhenInputStreamIsCsv() throws IOException {
+        InputStream inputStream = getResourceInputStream("goodreadsImportSample.csv");
+        List<GoodreadsBookImport> goodreadsBookImports =
+                CsvUtils.read(inputStream, GoodreadsBookImport.class);
+        assertEquals(5, goodreadsBookImports.size());
+    }
+
+    private InputStream getResourceInputStream(String file) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
+    }
+}
