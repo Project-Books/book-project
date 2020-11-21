3d16e28a91f87832c021fbec796f9e2e396221c2
Rahul Yadav <rahulyadav7477@gmail.com>
2020-11-21 22:30:52 +0530

    1. Added copyright notice in the new files 2. Refactored assertion to use AssertJ

--- src/test/java/com/karankumar/bookproject/backend/utils/CsvUtilsTest.java
+++ src/test/java/com/karankumar/bookproject/backend/utils/CsvUtilsTest.java
@@ -1,14 +1,32 @@
+/*
+ * The book project lets a user keep track of different books they would like to read, are currently
+ * reading, have read or did not finish.
+ * Copyright (C) 2020  Karan Kumar
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.karankumar.bookproject.backend.utils;
 
 import com.karankumar.bookproject.backend.dto.GoodreadsBookImport;
 import com.karankumar.bookproject.backend.util.CsvUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CsvUtilsTest {
 
@@ -17,7 +35,7 @@ class CsvUtilsTest {
         InputStream inputStream = getResourceInputStream("goodreadsImportSample.csv");
         List<GoodreadsBookImport> goodreadsBookImports =
                 CsvUtils.read(inputStream, GoodreadsBookImport.class);
-        assertEquals(5, goodreadsBookImports.size());
+        assertThat(goodreadsBookImports.size()).isEqualTo(5);
     }
 
     private InputStream getResourceInputStream(String file) {
