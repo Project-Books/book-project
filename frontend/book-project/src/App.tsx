459b0f5e52ae4efb6f14f03e8dfa972742322644
Karan <karan@karankumar.com>
2020-12-25 13:57:15 +0000

    Remove Vaadin files

--- frontend/book-project/src/App.tsx
+++ frontend/book-project/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <h1 id="app-name">
           Book Project
         </h1>
 
--- v0.1.0/src/main/java/com/karankumar/bookproject/BookProjectApplication.java
+++ v0.1.0/src/main/java/com/karankumar/bookproject/BookProjectApplication.java
@@ -19,10 +19,9 @@ package com.karankumar.bookproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@SpringBootApplication
 public class BookProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
