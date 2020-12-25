178664847aae682f5ccffb410fe6c7932bab2b1f
Karan <karan@karankumar.com>
2020-12-25 12:54:35 +0000

    Update login screen

--- frontend/book-project/src/App.tsx
+++ frontend/book-project/src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Book Project
-        </p>
+        </h1>
+
+        <TextField id="standard-basic" label="Username" />
+
+        <br />
+        <br />
+
+        <TextField 
+          id="standard-basic" 
+          label="Password" 
+          type="password"
+        />
+
+        <br />
+        <br />
+
+        <Button id="login" variant="contained" color="primary">
+          Log in
+        </Button>
+        
       </header>
     </div>
   );
