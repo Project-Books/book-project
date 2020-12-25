086da4f5d65f7d67ddc9aa65daf9e4555c341669
Karan <karan@karankumar.com>
2020-12-25 14:36:42 +0000

    Remove redundant frontend directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
 
 function App() {
   return (
