0774d7207d110556888c07f84131d60dc08e5d1c
Karan <karan@karankumar.com>
2021-01-23 07:59:50 +0000

    Make buttons black

---
+++ frontend/src/shared/theme.tsx
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme  = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#000000",
+        }
+    }
+})
