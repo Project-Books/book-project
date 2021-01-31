2beda1a070b5bb61648c4c0106a3c3b5e931c06e
Daniel <github@hoeller-d.at>
2021-01-31 10:38:59 +0100

    Connect the login view to the backend

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -15,25 +15,28 @@ You should have received a copy of the GNU General Public License along with thi
 If not, see <https://www.gnu.org/licenses/>.
 */
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Login.css'
 import Button from '@material-ui/core/Button'
 import Password from '../shared/form/Password'
 import EmailAddress from '../shared/form/EmailAddress'
 import { Link } from "react-router-dom"
 import logo from '../shared/media/logo/logo-black.png'
+import Endpoints from '../shared/api/endpoints'
+import Verb from '../shared/http/verb'
+import { RouteComponentProps } from 'react-router-dom';
 
 interface IState {
   email: string,
   password: string,
   isEmailDirty: boolean,
-  areCredentialsInvalid: boolean
+  areCredentialsInvalid: boolean,
+  loginFailed: boolean
 }
 
 type LoginProps = {
-}
-
-class Login extends Component<{}, IState> {
+} & RouteComponentProps
+class Login extends Component<LoginProps, IState> {
   constructor(props: LoginProps) {
     super(props)
 
@@ -41,18 +44,21 @@ class Login extends Component<{}, IState> {
       email: '',
       password: '',
       isEmailDirty: false,
-      areCredentialsInvalid: false
+      areCredentialsInvalid: false,
+      loginFailed: false
     }
 
     this.onPasswordChanged = this.onPasswordChanged.bind(this)
     this.onEmailChanged = this.onEmailChanged.bind(this);
     this.onClickLogin = this.onClickLogin.bind(this)
+    this.sendLoginRequest = this.sendLoginRequest.bind(this)
+    this.sendLoginRequestIfCredentialsAreValid = this.sendLoginRequestIfCredentialsAreValid.bind(this)
   }
 
   onClickLogin() {
     this.setState({
       areCredentialsInvalid: this.state.email === '' && this.state.password === ''
-    })
+    }, this.sendLoginRequestIfCredentialsAreValid)
   }
 
   onEmailChanged(email: string) {
@@ -72,10 +78,46 @@ class Login extends Component<{}, IState> {
     return isEmailDirtyAndBlank || this.state.areCredentialsInvalid
   }
   
+  sendLoginRequestIfCredentialsAreValid() {
+    if (!this.state.areCredentialsInvalid) {
+      this.sendLoginRequest()
+    }
+  }
+
+  sendLoginRequest(): void {
+    const requestOptions = {
+      method: Verb.POST,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.email,
+        password: this.state.password
+      })
+    }
+
+    fetch(Endpoints.login, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          this.props.history.push('/my-books')
+        } else {
+          this.setState({loginFailed: true});
+        }
+      })
+      .catch(error => {
+        this.setState({loginFailed: true})
+        console.error('error: ', error)
+      })
+  }
+
+  renderLoginError() {
+    return <p style={{ color: 'red' }}>Your email or password is incorrect. Please try again</p>
+  }
+
   render() {
     return (
       <div >
-          <img src={logo} alt="Logo" id="app-logo"/>
+        <img src={logo} alt="Logo" id="app-logo" />
 
           <br />
           <br />
@@ -116,6 +158,9 @@ class Login extends Component<{}, IState> {
             <Button className="login" id="createAccount" component={Link} to="/sign-up">
               Create account
             </Button>
+
+          {this.state.loginFailed && this.renderLoginError()}
+
           </div>
       </div>
     )
