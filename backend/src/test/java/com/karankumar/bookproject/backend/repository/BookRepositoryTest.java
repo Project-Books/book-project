8dcbb0d1c9f88e68a1939908faacd84261e5a8f3
kateerfle <erflek@dickinson.edu>
2021-02-17 09:02:14 -0500

    able to compile the backend source and tests, but now 134 tests are failing

--- backend/src/test/java/com/karankumar/bookproject/backend/repository/BookRepositoryTest.java
+++ backend/src/test/java/com/karankumar/bookproject/backend/repository/BookRepositoryTest.java
@@ -2,15 +2,12 @@
  * The book project lets a user keep track of different books they would like to read, are currently
  * reading, have read or did not finish.
  * Copyright (C) 2020  Karan Kumar
-
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU General Public License for more details.
-
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
@@ -28,7 +25,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.karankumar.bookproject.util.SecurityTestUtils.getTestUser;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -73,13 +69,13 @@ class BookRepositoryTest {
     void successfullyDeleteABook_whenAuthorHasOtherBooks() {
         // given
         bookRepository.saveAndFlush(new Book("Book2", author, read));
-        Optional<Book> book = bookRepository.findByTitleOrAuthor("title", WILDCARD).get(0);
+        Book book = bookRepository.findByTitleOrAuthor("title", WILDCARD).get(0);
 
         // when
         bookRepository.delete(book);
 
         // then
-        assertThat(bookRepository.findByTitleOrAuthor(WILDCARD, "firstName").count())
+        assertThat(bookRepository.findByTitleOrAuthor(WILDCARD, "firstName").size())
                 .isOne();
     }
 
@@ -191,7 +187,7 @@ class BookRepositoryTest {
             softly.assertThat(bookRepository.findByTitleOrAuthor(WILDCARD, firstName).size())
                   .isOne();
             softly.assertThat(bookRepository.findByTitleOrAuthor(WILDCARD, lastName).size())
-            	  .isOne();
+                  .isOne();
         });
     }
 }
