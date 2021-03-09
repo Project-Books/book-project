25e36005cc96bf302b60d56ead3bc45ef03e3188
hsergiu <57728877+hsergiu@users.noreply.github.com>
2021-03-09 09:21:08 +0200

    Show error messages on log in and create account clicked

--- frontend/src/register/Register.tsx
+++ frontend/src/register/Register.tsx
@@ -16,8 +16,8 @@ if not, see <https://www.gnu.org/licenses/>.
 */
 
 import Button from '@material-ui/core/Button';
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React, {Component, ReactElement} from 'react';
+import {Link} from "react-router-dom";
 import zxcvbn from 'zxcvbn';
 import Endpoints from '../shared/api/endpoints';
 import EmailAddress from '../shared/form/EmailAddress';
@@ -28,171 +28,187 @@ import logo from '../shared/media/logo/logo_one_line@1x.png';
 import './Register.css';
 
 enum PassStrengthEnum {
-  WEAK,
-  FAIR,
-  GOOD,
-  STRONG,
-  VERY_STRONG
+    WEAK,
+    FAIR,
+    GOOD,
+    STRONG,
+    VERY_STRONG
 }
 
 type RegisterProps = {
 }
 
 interface IState {
-  email: string,
-  password: string,
-  passwordsMatch: boolean,
-  passwordStrengthScore?: number,
-  isEmailDirty: boolean,
-  isPasswordDirty: boolean,
-  areCredentialsInvalid: boolean
+    email: string,
+    password: string,
+    passwordConfirmed: string,
+    passwordStrengthScore?: number,
+    isEmailDirty: boolean,
+    isPasswordDirty: boolean,
+    isEmailInvalid: boolean,
+    isPasswordInvalid: boolean
 }
 
 class Register extends Component<Record<string, unknown>, IState> {
-  constructor(props: RegisterProps) {
-    super(props)
+    constructor(props: RegisterProps) {
+        super(props)
 
-    this.state = {
-      email: '',
-      password: '',
-      passwordsMatch: true,
-      passwordStrengthScore: undefined,
-      isEmailDirty: false,
-      isPasswordDirty: false,
-      areCredentialsInvalid: false
+        this.state = {
+            email: '',
+            password: '',
+            passwordConfirmed: '',
+            passwordStrengthScore: undefined,
+            isEmailDirty: false,
+            isPasswordDirty: false,
+            isEmailInvalid: false,
+            isPasswordInvalid: false
+        }
+
+        this.handlePasswordChanged = this.handlePasswordChanged.bind(this)
+        this.handleConfirmPasswordChanged = this.handleConfirmPasswordChanged.bind(this)
+        this.onEmailChanged = this.onEmailChanged.bind(this)
+        this.onCreateAccountClicked = this.onCreateAccountClicked.bind(this)
     }
 
-    this.handlePasswordChanged = this.handlePasswordChanged.bind(this)
-    this.handleConfirmPasswordChanged = this.handleConfirmPasswordChanged.bind(this)
-    this.onEmailChanged = this.onEmailChanged.bind(this);
-    this.onCreateAccountClicked = this.onCreateAccountClicked.bind(this)
-  }
+    handlePasswordChanged(password: string): void {
+        const score = password
+            ? zxcvbn(password).score
+            : undefined;
 
-  handlePasswordChanged(password: string): void {
-    const score = password
-      ? zxcvbn(password).score
-      : undefined;
-
-    this.setState({
-      password,
-      isPasswordDirty: true,
-      passwordStrengthScore: score
-    })
-  }
-
-  handleConfirmPasswordChanged(password: string): void {
-    const passwordsMatch = password === this.state.password
-    this.setState({ passwordsMatch })
-  }
-
-  onEmailChanged(email: string): void {
-    this.setState({
-      email,
-      isEmailDirty: true
-    })
-  }
-
-  onCreateAccountClicked(): void {
-    const isFieldEmpty = this.state.email === '' || this.state.password === ''
-    this.setState({
-      areCredentialsInvalid: isFieldEmpty
-    })
-
-
-    if (!this.state.areCredentialsInvalid && this.state.passwordStrengthScore === PassStrengthEnum.VERY_STRONG) {
-      this.sendRegisterRequest()
-    }
-  }
-
-  sendRegisterRequest(): void {
-    const requestOptions = {
-      method: Verb.POST,
-      headers: {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-      })
+        this.setState({
+            password,
+            isPasswordDirty: true,
+            isPasswordInvalid: password === '',
+            passwordStrengthScore: score
+        })
     }
 
-    fetch(Endpoints.register, requestOptions)
-      .then(response => response.json())
-      .then(data => console.log('data: ', data))
-      .catch(error => console.log('error: ', error))
-  }
+    handleConfirmPasswordChanged(password: string): void {
+        this.setState({passwordConfirmed: password})
+    }
 
-  isPasswordInvalid(): boolean {
-    const isPasswordDirtyAndBlank = this.state.password === '' && this.state.isPasswordDirty
-    return isPasswordDirtyAndBlank || this.state.areCredentialsInvalid
-  }
+    checkPasswordsMatch(): boolean {
+        return this.state.password === this.state.passwordConfirmed
+    }
 
-  render() {
-    return (
-      <div className="center-table">
-        <div className="center-table-cell">
-          <img src={logo} alt="Logo" className="center" id="app-logo" />
+    onEmailChanged(email: string): void {
+        this.setState({
+            email,
+            isEmailDirty: true,
+            isEmailInvalid: email === ''
+        })
+    }
 
-          <br />
-          <br />
-          <br />
-          
-            <div className="center">
-              <EmailAddress
-                class="center login"
-                isInvalid={this.state.areCredentialsInvalid}
-                onChange={this.onEmailChanged}
-                areCredentialsInvalid={this.state.areCredentialsInvalid}
-              />
-    
-              <br />
-    
-              <Password
-                fieldName={'Password'}
-                class={'center login'}
-                onPasswordChanged={this.handlePasswordChanged}
-                isInvalid={this.isPasswordInvalid()}
-                errorMessage={'Please enter a password'}
-              />
-              <br />
-              <br />
-    
-              <PasswordStrengthMeter score={this.state.passwordStrengthScore}></PasswordStrengthMeter>
-    
-              <br />
-    
-              <Password
-                fieldName={'Confirm password'}
-                class={'center login'}
-                onPasswordChanged={this.handleConfirmPasswordChanged}
-                isInvalid={!this.state.passwordsMatch || this.state.areCredentialsInvalid}
-                errorMessage={'Passwords do not match. Please try again'}
-              />
-    
-              <br />
-              <br />
-    
-              <Button
-                className="center login"
-                variant="contained"
-                color="primary"
-                onClick={this.onCreateAccountClicked}>
-                Create account
-              </Button>
-    
-              <br />
-              <br />
-    
-              <Button className="center login" id="createAccount" component={Link} to="/sign-in">
-                Sign in instead
-              </Button>
+    onCreateAccountClicked(): void {
+        this.setState({
+            isEmailInvalid: this.state.email === '',
+            isPasswordInvalid: this.state.password === ''
+        })
 
-          </div> 
-        </div>
-      </div>
-    )
-  }
+        if (!this.state.isEmailInvalid && !this.state.isPasswordInvalid
+            && this.checkPasswordsMatch() &&
+            this.state.passwordStrengthScore === PassStrengthEnum.VERY_STRONG) {
+            this.sendRegisterRequest()
+        }
+    }
+
+    sendRegisterRequest(): void {
+        const requestOptions = {
+            method: Verb.POST,
+            headers: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        }
+
+        fetch(Endpoints.register, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log('data: ', data))
+            .catch(error => console.log('error: ', error))
+    }
+
+    isEmailInvalid(): boolean {
+        const isEmailDirtyAndBlank = this.state.email === '' && this.state.isEmailDirty
+        return isEmailDirtyAndBlank || this.state.isEmailInvalid
+    }
+
+    isPasswordInvalid(): boolean {
+        const isPasswordDirtyAndBlank = this.state.password === '' && this.state.isPasswordDirty
+        return isPasswordDirtyAndBlank || this.state.isPasswordInvalid
+    }
+
+    render(): ReactElement {
+        return (
+            <div className="center-table">
+                <div className="center-table-cell">
+                    <img src={logo} alt="Logo" className="center" id="app-logo" />
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <div className="center">
+                        <EmailAddress
+                            class="center login"
+                            classHelper="center helper"
+                            isInvalid={this.isEmailInvalid()}
+                            onChange={this.onEmailChanged}
+                            errorMessage={'Please enter an e-mail address'}
+                        />
+
+                        <br />
+
+                        <Password
+                            fieldName={'Password'}
+                            class={'center login'}
+                            classHelper={'center helper'}
+                            onPasswordChanged={this.handlePasswordChanged}
+                            isInvalid={this.isPasswordInvalid()}
+                            errorMessage={'Please enter a password'}
+                        />
+                        <br />
+                        <br />
+
+                        <PasswordStrengthMeter score={this.state.passwordStrengthScore} />
+
+                        <br />
+
+                        <Password
+                            fieldName={'Confirm password'}
+                            class={'center login'}
+                            classHelper={'center helper'}
+                            onPasswordChanged={this.handleConfirmPasswordChanged}
+                            isInvalid={!this.checkPasswordsMatch() || this.state.isPasswordInvalid}
+                            errorMessage={'Passwords do not match. Please try again'}
+                        />
+
+                        <br />
+                        <br />
+
+                        <Button
+                            className="center login"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.onCreateAccountClicked}>
+                            Create account
+                        </Button>
+
+                        <br />
+                        <br />
+
+                        <Button className="center login" id="createAccount" component={Link} to="/sign-in">
+                            Sign in instead
+                        </Button>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Register
