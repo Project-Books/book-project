d25c0f0f7bff4f54b18a46cb04b469ff8d07dbf1
Kate Erfle <erflek@dickinson.edu>
2021-04-10 10:06:32 -0400

    Fix import for modelmapper matching strategies

--- backend/src/main/java/com/karankumar/bookproject/BookProjectApplication.java
+++ backend/src/main/java/com/karankumar/bookproject/BookProjectApplication.java
@@ -23,7 +23,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.Conditions;
-import org.modelmapper.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import com.karankumar.bookproject.backend.model.BookGenre;
 
