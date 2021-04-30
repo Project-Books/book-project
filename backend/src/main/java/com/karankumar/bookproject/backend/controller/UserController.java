f9d0359862fb1ebc94edd5f94b20a9e9ff2b35bb
Karan <karan@karankumar.com>
2021-04-30 18:57:19 +0100

    Remove wildcard import

--- backend/src/main/java/com/karankumar/bookproject/backend/controller/UserController.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/controller/UserController.java
@@ -19,7 +19,12 @@ import com.karankumar.bookproject.backend.service.InvalidOldPasswordException;
 import com.karankumar.bookproject.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
