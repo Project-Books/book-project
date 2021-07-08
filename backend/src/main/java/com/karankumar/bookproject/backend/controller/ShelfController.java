63e575e54337903fc85ab62805845833fb0b4afc
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-07-08 01:27:20 +0100

    get user created shelf by id

--- backend/src/main/java/com/karankumar/bookproject/backend/controller/ShelfController.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/controller/ShelfController.java
@@ -79,13 +79,23 @@ public class ShelfController {
 
 
 
-    @GetMapping("/custom/all")
+    @GetMapping("/user-created/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserCreatedShelf getUserCreatedShelfById(@PathVariable("id") Long shelfId){
+        String errorMessage = String.format("no shelf matches the shelf name: %d",
+                shelfId);
+        userCreatedShelfService.findById(shelfId).orElseThrow(
+                () -> new IllegalStateException(errorMessage)
+        );
+        return null;
+    }
+    @GetMapping("/user-created/all")
     @ResponseStatus(HttpStatus.OK)
     public List<UserCreatedShelf> getAllCustomShelf(){
         return userCreatedShelfService.findAll();
     }
 
-    @GetMapping("/custom/all")
+    @GetMapping("/user-created/all")
     @ResponseStatus(HttpStatus.OK)
     public List<UserCreatedShelf> getAllCustomShelf(@RequestParam String shelfName) {
         List<UserCreatedShelf> allShelvesByName = userCreatedShelfService.findAll(shelfName);
