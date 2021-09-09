d9e7a6bd4224bd5ec0c26fd2d4b8c2756eb8165f
Karan <karan@karankumar.com>
2021-09-09 16:24:12 +0100

    Make no arg UserToDeleteDto constructor private and all args mandatory

--- backend/src/main/java/com/karankumar/bookproject/backend/dto/UserToDeleteDto.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/dto/UserToDeleteDto.java
@@ -14,11 +14,16 @@
 
 package com.karankumar.bookproject.backend.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserToDeleteDto {
     private String password;
 }
