9b46e3c5303f8281fd3b369c00d7235b54e97bdc
Petr Kadlec <kadlec@2n.cz>
2021-10-30 19:39:25 +0200

    Optimized imports, no * wildcards and code reformat according to project style guide

--- backend/src/main/java/com/karankumar/bookproject/backend/model/Author.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/model/Author.java
@@ -1,72 +1,79 @@
 /*
-    The book project lets a user keep track of different books they would like to read, are currently
-    reading, have read or did not finish.
-    Copyright (C) 2020  Karan Kumar
+   The book project lets a user keep track of different books they would like to read, are currently
+   reading, have read or did not finish.
+   Copyright (C) 2020  Karan Kumar
 
-    This program is free software: you can redistribute it and/or modify it under the terms of the
-    GNU General Public License as published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify it under the terms of the
+   GNU General Public License as published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-    PURPOSE.  See the GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with this program.
-    If not, see <https://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License along with this program.
+   If not, see <https://www.gnu.org/licenses/>.
+*/
 
 package com.karankumar.bookproject.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
 @JsonIgnoreProperties(value = {"id", "books"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@NamedEntityGraph(name = "Author.books",
-        attributeNodes = @NamedAttributeNode("books")
-)
+@NamedEntityGraph(name = "Author.books", attributeNodes = @NamedAttributeNode("books"))
 public class Author {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  private Long id;
 
-    @NotBlank
-    private String fullName;
+  @NotBlank private String fullName;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "author")
-    @Setter
-    @ToString.Exclude
-    private Set<Book> books = new HashSet<>();
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "author")
+  @Setter
+  @ToString.Exclude
+  private Set<Book> books = new HashSet<>();
 
-    public Author(String fullName) {
-        this.fullName = fullName;
-    }
+  public Author(String fullName) {
+    this.fullName = fullName;
+  }
 
-    @Override
-    public String toString() {
-        return fullName;
-    }
+  @Override
+  public String toString() {
+    return fullName;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return fullName.equals(author.fullName);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Author author = (Author) o;
+    return fullName.equals(author.fullName);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(fullName);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(fullName);
+  }
 }
--- backend/src/main/java/com/karankumar/bookproject/backend/model/account/Role.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/model/account/Role.java
@@ -1,31 +1,34 @@
 /*
-    The book project lets a user keep track of different books they would like to read, are currently
-    reading, have read or did not finish.
-    Copyright (C) 2020  Karan Kumar
+   The book project lets a user keep track of different books they would like to read, are currently
+   reading, have read or did not finish.
+   Copyright (C) 2020  Karan Kumar
 
-    This program is free software: you can redistribute it and/or modify it under the terms of the
-    GNU General Public License as published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify it under the terms of the
+   GNU General Public License as published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-    PURPOSE.  See the GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with this program.
-    If not, see <https://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License along with this program.
+   If not, see <https://www.gnu.org/licenses/>.
+*/
 
 package com.karankumar.bookproject.backend.model.account;
 
-import lombok.*;
-import org.hibernate.Hibernate;
-
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -33,29 +36,28 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  private Long id;
 
-    // TODO: use RoleType here instead
-    @NotBlank
-    private String role;
+  // TODO: use RoleType here instead
+  @NotBlank private String role;
 
-    public Role(@NonNull String role) {
-        this.role = role;
-    }
+  public Role(@NonNull String role) {
+    this.role = role;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role1 = (Role) o;
-        return role.equals(role1.role);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Role role1 = (Role) o;
+    return role.equals(role1.role);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(role);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(role);
+  }
 }
