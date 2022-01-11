a5d76bc25d54f4a30f4a2798aeae103ba5387e51
Karan <karan.n.j.kumar@gmail.com>
2022-01-11 18:53:46 +0000

    Remove wildcard import

--- backend/book-app/src/main/java/com/karankumar/bookproject/service/UserService.java
+++ backend/book-app/src/main/java/com/karankumar/bookproject/service/UserService.java
@@ -40,7 +40,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
@@ -48,7 +47,10 @@ import javax.validation.Validator;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
