0ef621391dce0d9cafbdbda8b2f6d82c81b6d1a3
Gabriel Ronei <gabriheron@hotmail.com>
2022-01-31 01:12:46 -0300

    Adding fake mailer to dev profile

---
+++ backend/email-service/src/main/java/com/karankumar/bookproject/book/service/FakeEmailService.java
@@ -0,0 +1,32 @@
+package com.karankumar.bookproject.book.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Profile({"dev", "test"})
+@Component
+public class FakeEmailService implements EmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FakeEmailService.class);
+
+    @Override
+    public void sendSimpleMessage(String from, String to, String message) {
+        LOGGER.info("[Mail] email sent to -> {}", to);
+        System.out.println("----------------------------------------- Simple Message Mail Sent -----------------------------------------");
+        System.out.println("From: " + from);
+        System.out.println("To: " + to);
+        System.out.println("Message: " + message);
+    }
+
+    @Override
+    public void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel) {
+        LOGGER.info("[Mail] email sent to -> {}", to);
+        System.out.println("----------------------------------------- Send Message with thymleaf -----------------------------------------");
+        System.out.println("To: " + to);
+        System.out.println("Subject: " + subject);
+    }
+}
