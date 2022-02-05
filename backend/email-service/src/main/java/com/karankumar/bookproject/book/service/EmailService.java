72f3e24f3ca58cf4a619de9f7ef1818d80806abb
Karan <karan.n.j.kumar@gmail.com>
2022-02-05 19:27:48 +0000

    Remove redundant modifier

--- backend/email-service/src/main/java/com/karankumar/bookproject/book/service/EmailService.java
+++ backend/email-service/src/main/java/com/karankumar/bookproject/book/service/EmailService.java
@@ -20,11 +20,12 @@ import java.util.Map;
 public interface EmailService {
 
     void sendSimpleMessage(String from, String to, String message);
-    void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel)
-            throws MessagingException;
+    void sendMessageUsingThymeleafTemplate(
+            String to, String subject, Map<String, Object> templateModel
+    ) throws MessagingException;
 
 
-    default public String getUsernameFromEmail(String email){
+    default String getUsernameFromEmail(String email) {
         return email.substring(0, email.indexOf("@"));
     }
 }
