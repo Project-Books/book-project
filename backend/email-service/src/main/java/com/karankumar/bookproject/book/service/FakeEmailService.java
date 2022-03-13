7f50184005e870952cef1ffbf42fd970da467379
Karan <karan.n.j.kumar@gmail.com>
2022-03-13 13:38:57 +0000

    chore: rename dev env to local

--- backend/email-service/src/main/java/com/karankumar/bookproject/book/service/FakeEmailService.java
+++ backend/email-service/src/main/java/com/karankumar/bookproject/book/service/FakeEmailService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Profile({"dev", "test"})
+@Profile({"local", "test"})
 @Component
 @ExcludeFromJacocoGeneratedReport
 public class FakeEmailService implements EmailService {
