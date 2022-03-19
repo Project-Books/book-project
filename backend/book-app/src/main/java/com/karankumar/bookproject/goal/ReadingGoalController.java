d00aa112d00d12929e3717e4a7c34db22a480447
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-03-20 04:14:29 +0530

    chore: workaround for failing tests
    adds a exeption handler for ReadingGoalController failing tests to respond with exceptions correctly

--- backend/book-app/src/main/java/com/karankumar/bookproject/goal/ReadingGoalController.java
+++ backend/book-app/src/main/java/com/karankumar/bookproject/goal/ReadingGoalController.java
@@ -19,18 +19,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
-import static org.mockito.Mockito.mock;
 
 @RestController
 @RequestMapping(Mappings.GOAL)
@@ -92,11 +91,39 @@ public class ReadingGoalController {
     return readingGoalService.findAll();
   }
 
-  @GetMapping(Endpoints.CURRENT)
-  public ReadingGoal getExistingReadingGoal() {
-    return readingGoalService.findAll().stream()
-        .findFirst()
-        .orElseThrow(
-            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, READING_GOAL_NOT_FOUND));
-  }
+        ReadingGoal readingGoal = new ReadingGoal(target, ReadingGoal.GoalType.PAGES);
+        readingGoalService.save(readingGoal);
+    }
+
+    // This endpoint will be used for both adding and updating, since at most one goal
+    // (pages or books) is allowed at a time
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(Endpoints.ADD_PAGES)
+    public void addPagesReadingGoal(@RequestParam(value = "target") int target) {
+        if (target <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, TARGET_BAD_REQUEST);
+        }
+
+        ReadingGoal readingGoal = new ReadingGoal(target, ReadingGoal.GoalType.PAGES);
+        readingGoalService.save(readingGoal);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(Endpoints.PREVIOUS)
+    public List<ReadingGoal> getPreviousReadingGoals() {
+        return readingGoalService.findAll();
+    }
+
+    @GetMapping(Endpoints.CURRENT)
+    public ReadingGoal getExistingReadingGoal() {
+        return readingGoalService.findAll().stream().findFirst().orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, READING_GOAL_NOT_FOUND)
+        );
+    }
+
+    @ExceptionHandler(value = {ResponseStatusException.class})
+    public ResponseEntity<String> unknownException(ResponseStatusException ex, WebRequest req) {
+        return ResponseEntity.status(ex.getStatus()).body(ex.getReason());
+    }
+
 }
