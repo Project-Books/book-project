fdfd892549b6cec1bd1d52943f64c6ba91f326bb
Girish-Bharadwaj <girishkulkarni351@gmail.com>
2022-03-20 09:23:45 +0530

    create statistics page

--- frontend/src/statistics/Stats.tsx
+++ frontend/src/statistics/Stats.tsx
@@ -17,10 +17,71 @@ If not, see <https://www.gnu.org/licenses/>.
 
 import React from 'react'
 import { Layout } from '../shared/components/Layout';
+import { LibraryBooks, MenuBookSharp, Star, StarOutlined } from '@material-ui/icons';
+import './Stats.css'
+import { ThumbUp } from '@material-ui/icons';
+
 
 function Stats(): JSX.Element {
     return (
-        <Layout title="Statistics"></Layout>
+        <Layout title="Statistics" centered={true}>
+            <div className="books-list">
+                <div className='book-container'>
+                    <h3 className='book-title'>Most liked book</h3>
+                    <img src="https://inliterature.net/wp-content/uploads/2014/04/harry-potter-1-709x1024.jpg" className='image' />
+                    <div className="book-rating">
+                        <StarOutlined className='star-icon' fontSize='small' />
+                        <p className='rating-number'>8</p>
+                    </div>
+                </div>
+                <div className='book-container'>
+                    <h3 className='book-title'>Least liked book</h3>
+                    <img src="https://inliterature.net/wp-content/uploads/2014/04/harry-potter-1-709x1024.jpg" className='image' />
+                    <div className="book-rating red">
+                        <StarOutlined className='star-icon' fontSize='small' />
+                        <p className='rating-number'>3</p>
+                    </div>
+                </div>
+                <div className='book-container'>
+                    <h3 className='book-title'>Longest book read</h3>
+                    <img src="https://inliterature.net/wp-content/uploads/2014/04/harry-potter-1-709x1024.jpg" className='image' />
+                    <div className="book-rating black">
+                        <p className='rating-number'>628 pages</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="statistics-container">
+                <div className='stat-container'>
+                    <MenuBookSharp fontSize='large' />
+                    <div className="content">
+                        <h5 className='content-heading'>Most read genre</h5>
+                        <p className='content-desc'>biography</p>
+                    </div>
+                </div>
+                <div className='stat-container'>
+                    <Star fontSize='large' />
+                    <div className="content">
+                        <h5 className='content-heading'>Average rating given</h5>
+                        <p className='content-desc'>4.5/5</p>
+                    </div>
+                </div>
+                <div className='stat-container'>
+                    <ThumbUp fontSize='large' />
+                    <div className="content">
+                        <h5 className='content-heading'>Most liked genre</h5>
+                        <p className='content-desc'>biography</p>
+                    </div>
+                </div>
+                <div className='stat-container'>
+                    <LibraryBooks fontSize='large' />
+                    <div className="content">
+                        <h5 className='content-heading'>Average pages read</h5>
+                        <p className='content-desc'>368 pages</p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
