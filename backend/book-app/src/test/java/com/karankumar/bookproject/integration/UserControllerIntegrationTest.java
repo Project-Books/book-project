a62a236720881fe9dc222a55aebec9072d95e801
Erin <erin.e.mcconnell@vanderbilt.edu>
2022-05-05 22:44:45 -0500

    add weak and strong parameterized password tests, update ascii doc

--- backend/book-app/src/test/java/com/karankumar/bookproject/integration/UserControllerIntegrationTest.java
+++ backend/book-app/src/test/java/com/karankumar/bookproject/integration/UserControllerIntegrationTest.java
@@ -21,6 +21,8 @@ import com.karankumar.bookproject.account.dto.UserToRegisterDto;
 import com.karankumar.bookproject.BookProjectApplication;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,48 +35,97 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = BookProjectApplication.class)
+
+@SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(outputDir = "../target/snippets")
 @Tag("Integration")
 class UserControllerIntegrationTest {
-  @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  void canLoginWithTestUser() throws Exception {
-    // given
-    String url = "http://localhost:8080/login";
+    @Test
+    void canLoginWithTestUser() throws Exception {
+        // given
+        String url = "http://localhost:8080/login";
 
-    UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user@user.user", "password");
+        UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user@user.user", "password");
 
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-    ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
-    String requestJson = writer.writeValueAsString(userToRegisterDto);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = writer.writeValueAsString(userToRegisterDto);
 
-    // when & then
-    this.mockMvc
-        .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andDo(document("login"));
-  }
+        // when & then
+        this.mockMvc
+                .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("login"));
+    }
 
-  @Test
-  void shouldNotLoginIfWrongCredentials() throws Exception {
-    String url = "http://localhost:8080/login";
-    UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user@user.user", "userPassword");
+    @Test
+    void shouldNotLoginIfWrongCredentials() throws Exception {
+        String url = "http://localhost:8080/login";
+        UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user@user.user", "userPassword");
 
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-    ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
-    String requestJson = writer.writeValueAsString(userToRegisterDto);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = writer.writeValueAsString(userToRegisterDto);
 
-    // when & then
-    this.mockMvc
-        .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
-        .andDo(print())
-        .andExpect(status().is4xxClientError())
-        .andDo(document("login"));
-  }
+        // when & then
+        this.mockMvc
+                .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andDo(document("login"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"alphabeta", " ", "password", "p@ssword", "password1", "12345", "qwert12345", "helloworld",
+            "aweoijr", "Nh1-orWL", "ZBciM|1J", "Vl1u-DFw", "ntzyj5", "aXog*f", "BoXEvYqY", "6scb1x16", "-%%?|$@^",
+            "64186660", "+&9=2^", "PASSWORD", "pass word"})
+    void cannotRegisterWithWeakPassword(String password) throws Exception {
+        String url = "http://localhost:8080/api/user";
+
+        // set up user with given username and password
+        UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user1@user.user", password);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = writer.writeValueAsString(userToRegisterDto);
+
+        // expect error if password is weak
+        this.mockMvc
+                .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andDo(document("register-with-weak-password"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"bzoYMvAhIGSP", "hsdtcaslaqcyxiisa", "fmL9398#i^Uo", "2E^U9|Dg56", "-^XhKFWcwJ",
+            "r1lz8BmAVt", "PL!MeS34#*", "Pa332w0r9", "ISDJFISOPS", "&^=?$&?%-!", "1434987243", "23&?7&^96",
+            "password11298735", "hello there", "kermit the frog"})
+    void canRegisterWithStrongPassword(String password) throws Exception {
+        String url = "http://localhost:8080/api/user";
+
+        // set up user with given username and password
+        UserToRegisterDto userToRegisterDto = new UserToRegisterDto("user@user.user", password);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = writer.writeValueAsString(userToRegisterDto);
+
+        // expect strong password to be accepted
+        this.mockMvc
+                .perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("register-with-strong-password"));
+    }
+
 }
