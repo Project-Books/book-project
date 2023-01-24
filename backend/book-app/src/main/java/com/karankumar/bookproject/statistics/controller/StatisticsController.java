f5794c637f414f507b4a8d5d1569be8f50937b08
Roshan Mathew <roshan98@terpmail.umd.edu>
2023-01-23 20:40:32 -0500

    corrected statistics controller API

--- backend/book-app/src/main/java/com/karankumar/bookproject/statistics/controller/StatisticsController.java
+++ backend/book-app/src/main/java/com/karankumar/bookproject/statistics/controller/StatisticsController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/statitics")
+@RequestMapping("/api/statistics")
 public class StatisticsController {
 
   @Autowired
