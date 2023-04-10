42c18766ec2109776025a923c91b2cce7fc79a27
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2023-04-10 07:05:56 +0530

    Change from bash to shell

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting the frontend..."
 
 profile="$(printenv profile_env)"
 echo "$profile"
 
-if [ "$profile" == "local" ]
-then
+if [ "$profile" = "local" ]; then
     echo "Starting the local environment..."
     yarn start
-elif [ "$profile" == "dev" ]
-then
+elif [ "$profile" = "dev" ]; then
     echo "Starting the dev environment..."
     yarn build
 else
